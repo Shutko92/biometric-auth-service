048792bb02435dc76ef3500caefbba4e0faadb7b
Shutko <ilya.shutko@gmail.com>
2025-06-03 15:24:21 +0300

    reformat sms service

--- src/main/java/com/pm/biometric_auth_service/service/SmsService.java
+++ src/main/java/com/pm/biometric_auth_service/service/SmsService.java
@@ -1,25 +1,13 @@
 package com.pm.biometric_auth_service.service;
 
-import com.twilio.http.TwilioRestClient;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class SmsService {
-    @Value("${twilio.phone-number}")
-    private String twilioNumber;
-
-    @Autowired
-    private TwilioRestClient twilioClient;
 
     public void sendSms(String to, String message) {
-        PhoneNumber toNumber = new PhoneNumber(to);
-        PhoneNumber fromNumber = new PhoneNumber(twilioNumber);
-
-        Message.creator(toNumber, fromNumber, message)
-                .create(twilioClient);
+       log.info("Sending SMS with message {} to {}", message, to);
     }
 }
